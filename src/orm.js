0cbbd640ce7371c4d3b57fd7faed3482bfbee4a3
Cristian <cristianvasquez@gmail.com>
2024-03-31 05:45:34 +0200

    Simple ORM

---
+++ src/orm.js
@@ -0,0 +1,23 @@
+import rdf from 'rdf-ext'
+
+function toORM (dataset) {
+  const subjects = dataset.reduce((set, quad) => set.add(quad.subject),
+    rdf.termSet())
+  const connectSubjects = (quad) => subjects.has(quad.subject) &&
+    subjects.has(quad.object)
+  const nodes = [...subjects].map(
+    subject => ({
+      subject,
+      dataset: dataset.filter(
+        quad => quad.subject.equals(subject) && !connectSubjects(quad)),
+    }),
+  )
+  // const edges = [...dataset.filter(connectSubjects)]
+  const edges = dataset.filter(connectSubjects)
+  return {
+    nodes,
+    edges,
+  }
+}
+
+export { toORM }
